6b07846b272fed0b04e9763cfdb72b3b244e6f85
George Fabunmi <otegbeyegeorge1@gmail.com>
2026-01-03 21:20:29 +0100

    feat: implement sidebar navigation links and active state logic

--- components/ui/Sidebar.tsx
+++ components/ui/Sidebar.tsx
@@ -1,7 +1,12 @@
+"use client";
+import { sidebarLinks } from "@/constants";
 import Link from "next/link";
 import Image from "next/image";
+import { cn } from "@/lib/utils";
+import { usePathname } from "next/navigation";
 
 const Sidebar = ({ user }: SiderbarProps) => {
+  const pathName = usePathname();
   return (
     <section className="sidebar">
       <nav className="flex flex-col gap-4">
@@ -13,7 +18,22 @@ const Sidebar = ({ user }: SiderbarProps) => {
             alt="Nova Logo"
             className="size-[24px] max-xl:size-14"
           ></Image>
+          <h1 className="sidebar-log">Nova</h1>
         </Link>
+
+        {sidebarLinks.map((item) => {
+          const isActive =
+            pathName === item.route || pathName.startsWith(`${item.route}/`);
+          return (
+            <Link
+              href={item.route}
+              key={item.label}
+              className={cn("sidebar-link", { "bg-bank-gradient": isActive })}
+            >
+              {item.label}
+            </Link>
+          );
+        })}
       </nav>
     </section>
   );
