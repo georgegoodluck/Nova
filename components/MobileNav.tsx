6b43c287fd32b6278341380844158073d6a4f6f5
George Fabunmi <otegbeyegeorge1@gmail.com>
2026-01-08 20:14:44 +0100

    fix(ui): move SheetClose inside map loop to resolve scope error

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -4,8 +4,6 @@ import {
   Sheet,
   SheetClose,
   SheetContent,
-  SheetDescription,
-  SheetHeader,
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
@@ -17,20 +15,22 @@ import { sidebarLinks } from "@/constants";
 
 const MobileNav = ({ user }: MobileNavProps) => {
   const pathName = usePathname();
+
   return (
-    <section className="w-full max-w-[24px]1">
+    <section className="w-full max-w-[264px]">
       <Sheet>
         <SheetTrigger>
           <Image
             src="/icons/hamburger.svg"
-            width={24}
-            height={24}
+            width={30}
+            height={30}
             alt="menu"
             className="cursor-pointer"
-          ></Image>
+          />
         </SheetTrigger>
         <SheetContent side="left" className="border-none bg-white">
           <SheetTitle className="sr-only">Mobile Navigation</SheetTitle>
+
           <Link
             href="/"
             className="mb-12 cursor-pointer flex items-center gap-1 px-4"
@@ -46,43 +46,47 @@ const MobileNav = ({ user }: MobileNavProps) => {
               Nova
             </h1>
           </Link>
+
           <div className="mobilenav-sheet">
-            <SheetClose asChild>
-              <nav className="flex h-full flex-col gap-6 pt-16">
-                {sidebarLinks.map((item) => {
-                  const isActive =
-                    pathName === item.route ||
-                    pathName.startsWith(`${item.route}/`);
-                  return (
+            <nav className="flex h-full flex-col gap-6 pt-16 text-white">
+              {sidebarLinks.map((item) => {
+                const isActive =
+                  pathName === item.route ||
+                  pathName.startsWith(`${item.route}/`);
+
+                return (
+                  <SheetClose asChild key={item.route}>
                     <Link
                       href={item.route}
-                      key={item.label}
-                      className={cn("sidebar-link", {
+                      className={cn("mobilenav-sheet_close w-full", {
                         "bg-bank-gradient": isActive,
                       })}
                     >
-                      <div className="relative size-6">
-                        <Image
-                          src={item.imgURL}
-                          alt={item.label}
-                          fill
-                          className={cn({
-                            "brightness-[3] invert-0": isActive,
-                          })}
-                        />
-                      </div>
+                      <Image
+                        src={item.imgURL}
+                        alt={item.label}
+                        width={20}
+                        height={20}
+                        className={cn({
+                          "brightness-[3] invert-0": isActive,
+                        })}
+                      />
                       <p
-                        className={cn("sidebar-label", {
-                          "!text-white": isActive,
+                        className={cn("text-16 font-semibold text-black-2", {
+                          "text-white": isActive,
                         })}
                       >
                         {item.label}
                       </p>
                     </Link>
-                  );
-                })}
-              </nav>
-            </SheetClose>
+                  </SheetClose>
+                );
+              })}
+              {/* USER INFO PLACEHOLDER */}
+              USER
+            </nav>
+            {/* FOOTER PLACEHOLDER */}
+            FOOTER
           </div>
         </SheetContent>
       </Sheet>
