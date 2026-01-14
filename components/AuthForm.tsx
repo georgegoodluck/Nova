214378b8e785eacb14442462f7f722010e6b39ed
George Fabunmi <otegbeyegeorge1@gmail.com>
2026-01-14 21:50:28 +0100

    fix: add use client directive to AuthForm to support state

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -1,9 +1,33 @@
-import React from 'react'
+'use client';
+import React, { useState } from "react";
+import Link from "next/link";
+import Image from "next/image";
 
 const AuthForm = ({ type }: { type: string }) => {
+  const [user, setUser] = useState(null);
   return (
-    <div>{type}</div>
-  )
-}
+    <section className="auth-form">
+      <header className="flex flex-col gap-5 md:gap-8">
+        <Link href="/" className="cursor-pointer flex items-center gap-1 px-4">
+          <Image
+            src="/icons/logo.svg"
+            width={34}
+            height={34}
+            alt="Nova logo"
+            className="size-[24px] max-xl:size-14"
+          />
+          <h1 className="text-26 font-ibm-plex-serif font-bold text-black-1">
+            Nova
+          </h1>
+        </Link>
+        <div className="flex flex-col gap-1 md:gap-3">
+          <h1>
+            {user ? "Link Account" : type === "sign-in" ? "Sign In" : "Sign Up"}
+          </h1>
+        </div>
+      </header>
+    </section>
+  );
+};
 
-export default AuthForm
+export default AuthForm;
