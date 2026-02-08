cb131f027f15857a49e7fa6f6e171f652f73f947
George Fabunmi <otegbeyegeorge1@gmail.com>
2026-02-08 18:33:40 +0100

    chore: implement dynamic custom input

--- components/CustomInput.tsx
+++ components/CustomInput.tsx
@@ -1,10 +1,10 @@
 import React from "react"
 import {
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import { Control, FieldPath } from "react-hook-form"
@@ -12,35 +12,36 @@ import { z } from "zod"
 import { authFormSchema } from "@/lib/utils"
 
 interface CustomInput {
-  control: Control<z.infer<typeof authFormSchema>>,
-  name: FieldPath<z.infer<typeof authFormSchema>>,
-  label: string,
-  placeholder: string;
+    control: Control<z.infer<typeof authFormSchema>>,
+    name: FieldPath<z.infer<typeof authFormSchema>>,
+    label: string,
+    placeholder: string;
 }
 
 const CustomInput = ({ control, name, label, placeholder }: CustomInput) => {
-  return (
-    <FormField control={control}
-      name={name}
-      render={({ field }) => (
-        <FormItem className='form-item'>
-          <FormLabel className='form-label'>
-            {label}
-          </FormLabel>
-          <div className="flex w-full flex-col">
-            <FormControl>
-              <Input
-                placeholder={placeholder}
-                className='input-class' type='password'
-                {...field}
-              />
-            </FormControl>
-            <FormMessage className='form-message mt-2' />
-          </div>
-        </FormItem>
-      )}
-    />
-  )
+    return (
+        <FormField control={control}
+            name={name}
+            render={({ field }) => (
+                <FormItem className='form-item'>
+                    <FormLabel className='form-label'>
+                        {label}
+                    </FormLabel>
+                    <div className="flex w-full flex-col">
+                        <FormControl>
+                            <Input
+                                placeholder={placeholder}
+                                className='input-class'
+                                type={name === 'password' ? 'password' : 'text'}
+                                {...field}
+                            />
+                        </FormControl>
+                        <FormMessage className='form-message mt-2' />
+                    </div>
+                </FormItem>
+            )}
+        />
+    )
 }
 
 export default CustomInput
